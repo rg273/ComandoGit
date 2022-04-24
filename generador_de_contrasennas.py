e4273144c7971e738c95ae766c3b92898c508f35
Rodrigo_LaptopI3_600U <rodrigogutierrez273@gmail.com>
2022-04-24 06:36:43 -0300

    generando proyecto(generador_contraseña)

--- generador_de_contrasennas.py
+++ generador_de_contrasennas.py
@@ -1,1 +1,14 @@
 import random #HERRAMIENTA MISTERIOSA QUE NOS SERVIRA MAS TARDE
+
+def run():
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+    def generador_contraseña(digitos):
+        i = 0
+        
+
+
+if __name__ =="__main__":
+    run()
